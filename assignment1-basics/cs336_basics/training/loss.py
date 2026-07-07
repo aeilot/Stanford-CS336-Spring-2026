16409967a00f09a4a95bbf19c936cec62ae79f33
Louis Deng <louisaeilotd@gmail.com>
2026-07-07 22:49:51 +0800

    Implement Perplexity

--- assignment1-basics/cs336_basics/training/loss.py
+++ assignment1-basics/cs336_basics/training/loss.py
@@ -1,8 +1,6 @@
 import torch
 from torch import nn
 
-from cs336_basics.transformers.module import Softmax
-
 
 # Defining CrossEntropy for Transformers LM
 # logits: (batch_size, vocab_size)
@@ -11,6 +9,14 @@ from cs336_basics.transformers.module import Softmax
 class CrossEntropyLoss(nn.Module):
     def forward(self, logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
         # CrossEntropy = lse - target_logits
+        # We should reduce the use of exp and log to avoid numerical instability.
+        # logits can be very large, which can lead to overflow when taking the exp.
         lse = torch.logsumexp(logits, dim=-1)
         target_logits = logits[torch.arange(logits.shape[0]), targets]
         return (lse - target_logits).mean()
+
+
+class Perplexity(nn.Module):
+    def forward(self, logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        loss = CrossEntropyLoss()(logits, targets)
+        return torch.exp(loss)
